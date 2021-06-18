896d359f274ba8affd67571fa94b9139d70a2d23
jmgoncalves97 <jonathanmattosgoncalves1@gmail.com>
2021-06-18 20:29:46 -0300

    add file OrdenandoNumerosParesImpares.java

--- a.challenges-dio/desafiosdio/src/main/java/com/desafiosdio/OrdenandoNumerosParesImpares.java
+++ a.challenges-dio/desafiosdio/src/main/java/com/desafiosdio/OrdenandoNumerosParesImpares.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class OrdenandoNumerosParesImpares {
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -14,13 +15,14 @@ public class OrdenandoNumerosParesImpares {
 
         int qtdNumeros = sc.nextInt();
 
-        var br = new BufferedReader(new InputStreamReader(System.in));
+        // var br = new BufferedReader(new InputStreamReader(System.in));
+        // var st = new StringTokenizer(br.readLine());
 
         List<Integer> numerosPares = new ArrayList<>();
         List<Integer> numerosImpares = new ArrayList<>();
 
         for (int i = 0; i < qtdNumeros; i++) {
-            int numero = Integer.parseInt(br.readLine());
+            int numero = sc.nextInt();
             if (numero % 2 == 0) {
                 numerosPares.add(numero);
             } else {
